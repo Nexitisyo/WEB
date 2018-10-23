4892db354ed98ae3c9cb10cf5ca78c9089d2f6a2
Nexitisyo <Nexitisyo@users.noreply.github.com>
2018-10-23 22:15:13 +0200

    Tutorial 6

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os, os.path
 import random
 import string
 
@@ -5,13 +6,40 @@ import cherrypy
 
 
 class StringGenerator(object):
-	@cherrypy.expose
-	def index(self):
-		return "Hello world!"
-		
-	@cherrypy.expose
-	def generate(self):
-		return ''.join(random.sample(string.hexdigits, 8))
-		
+    @cherrypy.expose
+    def index(self):
+        return """<html>
+          <head>
+            <link href="/static/css/style.css" rel="stylesheet">
+          </head>
+          <body>
+            <form method="get" action="generate">
+              <input type="text" value="8" name="length" />
+              <button type="submit">Give it now!</button>
+            </form>
+          </body>
+        </html>"""
+
+    @cherrypy.expose
+    def generate(self, length=8):
+        some_string = ''.join(random.sample(string.hexdigits, int(length)))
+        cherrypy.session['mystring'] = some_string
+        return some_string
+
+    @cherrypy.expose
+    def display(self)
+        return cherrypy.session['mystring']
+
+
 if __name__ == '__main__':
-	cherrypy.quickstart(StringGenerator())
+    conf = {
+        '/': {
+            'tools.sessions.on': True,
+            'tools.staticdir.root': os.path.abspath(os.getcwd())
+        },
+        '/static': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': './public'
+        }
+    }
+    cherrypy.quickstart(StringGenerator(), '/', conf)
