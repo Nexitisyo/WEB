acb91bac198d2e9b55be263923bdd8aa4579eb3c
Nexitisyo <Nexitisyo@users.noreply.github.com>
2018-10-30 19:51:52 +0100

    I am alive

--- p1/pro/app/app.py
+++ p1/pro/app/app.py
@@ -1,7 +1,16 @@
 import os
 from mako.template import Template
+from mako.lookup import TemplateLookup
 
 import cherrypy
+
+
+#return in der Cherrypy Methode returned Werte oder einen Inhalt von etwas.
+#In diesem Fall den Inhalt von Template welcher "I am alive" ist
+#Dort kann auch eine Zieldatei angegeben werden mit
+mytemplate = Template("I am alive")
+ #(filename='./template/liste.tpl')
+#rausfinden wie man in das übergeordnete Verzeichnis kommt
 
 """
 • Verwaltung der Kundendaten
@@ -14,10 +23,10 @@ import cherrypy
 		▪ Ort
 """
 
-class HelloWorld(object):
+class main(object):
 	@cherrypy.expose
 	def index(self):
-		mytemplate = Template("hello world!")
-		return print(mytemplate.render())
+		return mytemplate.render()
 
-cherrypy.quickstart(HelloWorld())
+
+cherrypy.quickstart(main())
